64c31a19ae47a94864e9799fb8550bcf9e9079dc
Hinson05 <113132293+Hinson05@users.noreply.github.com>
2023-01-05 13:02:23 -0500

    a

--- hinson/main.js
+++ hinson/main.js
@@ -1,7 +1,12 @@
 import "./style.css";
-const URL = "https://api.openweathermap.org/data";
- async function getdate(URL)  {
-   try {
-     const
-   }
- }
+const URL = "https://animechan.vercel.app/api/random";
+const Weather = "https://animechan.vercel.app/api/random/anime?title=naruto";
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    const data = await response.jason();
+    document.getElementById("api-response").textContent = console.log(response);
+  } catch (error) {}
+}
+
+getData();
