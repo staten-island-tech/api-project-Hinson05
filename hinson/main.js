fb9c347b833fa11f55e8019d234059314cdf29fb
Hinson05 <113132293+Hinson05@users.noreply.github.com>
2023-01-09 14:01:23 -0500

    a

--- hinson/main.js
+++ hinson/main.js
@@ -1,11 +1,15 @@
 import "./style.css";
+
 const URL = "https://animechan.vercel.app/api/random";
-const Anime = `https://animechan.vercel.app/api/random/character?name=${Name}`;
+const Anime = `https://animechan.vercel.app/api/random/character?name=${name}`;
 async function getData(URL) {
   try {
     const response = await fetch(URL);
-    const data = await response.jason();
-    document.getElementById("api-response").textContent = console.log(response);
+    const data = await response.json();
+    document.getElementById("api-response").textContent = console.log(
+      data.character
+    );
+    console.log(data);
   } catch (error) {}
 }
 
