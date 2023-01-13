b174f2934c7505634aea6b2baee899b144bfd0a3
Hinson05 <113132293+Hinson05@users.noreply.github.com>
2023-01-13 14:00:14 -0500

    a

--- hinson/main.js
+++ hinson/main.js
@@ -1,15 +1,25 @@
 const DOMSelectors = {
   submit: document.getElementById("submit"),
-  Name: document.getElementById("input"),
+  name: document.getElementById("input"),
+  list: document.getElementById("div"),
 };
-let URL = `https://api.nationalize.io/?name=${Name}`;
+let URL = `https://api.nationalize.io/?name=${name}`;
 async function getData() {
   try {
     const temp = await fetch(URL);
     const array = await temp.json();
     console.log(array);
+    let x = DOMSelectors.name.value;
+    DOMSelectors.list.insertAdjacentHTML(
+      "afterbegin",
+      `<p>class= "yes"> Name: ${x} </p> class= "yes"> Name: ${x} 
+      Nationality: ${array.country.country_id}</p>`
+    );
   } catch (error) {
     console.log(error);
   }
 }
-getData();
+
+DOMSelectors.submit.addEventListener("click", function () {
+  getData();
+});
