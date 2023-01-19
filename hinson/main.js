be8b017eb5a69d5fa54fccd335bfd0a24419e185
Hinson05 <113132293+Hinson05@users.noreply.github.com>
2023-01-19 13:59:19 -0500

    a

--- hinson/main.js
+++ hinson/main.js
@@ -6,15 +6,19 @@ const DOMSelectors = {
 
 async function getData() {
   try {
-    let URL = `https://api.nationalize.io/?name=${name}`;
-    const temp = await fetch(URL);
-    const array = await temp.json();
+    let URL = `https://api.nationalize.io/?name=${input.value}`;
+    const bleh = await fetch(URL);
+    const array = await bleh.json();
     console.log(array);
+    let flag = `https://countryflagsapi.com/png/${array.country[0].country_id}`;
+    const one = await fetch(flag);
+    console.log(one);
     let x = DOMSelectors.name.value;
     DOMSelectors.list.insertAdjacentHTML(
       "afterbegin",
       `<p class= "yes"> Name: ${x}  
-      Nationality: ${array.country.country_id} Probability: ${array.country.probability}</p>`
+      Nationality: ${array.country[0].country_id} Probability: ${array.country[0].probability}
+      <img src= 'https://countryflagsapi.com/png/${array.country[0].country_id}'></p>`
     );
   } catch (error) {
     console.log(error);
